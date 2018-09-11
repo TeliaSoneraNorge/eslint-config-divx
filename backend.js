f90f9c01554410b96932e1d18ea746f404e30b19
Tibor Vukovic <tiborvukovic91@gmail.com>
2018-09-11 11:56:25 +0200

    Format & update deps

--- backend.js
+++ backend.js
@@ -3,10 +3,10 @@ const mergeDeep = require('merge-deep');
 const common = require('./common');
 
 const config = {
-  "env": {
-    "node": true,
-    "mocha": true
-  }
+  env: {
+    node: true,
+    mocha: true,
+  },
 };
 
 module.exports = mergeDeep({}, common, config);
